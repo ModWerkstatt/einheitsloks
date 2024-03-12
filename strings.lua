5abb3617475a0addd3b2c833820ea2736fef9bf7
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-03-12 21:34:05 +0100

    Lomo hinzugefügt

--- strings.lua
+++ strings.lua
@@ -170,7 +170,7 @@ return {
         ["139.2_radve"] = "BR 139.2 Raildaventure 139 558 (2012)",
         ["139.2_byb"] = "BR 139.2 Bayernbahn 139 287 (2017)",
         ["139.2_byb2"] = "BR 139.2 Bayernbahn 139 309 (2020)",
-        ["139.2_lomo"] = "BR 139.2 Lokomotion (2020)",
+        ["139.2_lomo"] = "BR 139.2 Lokomotion 139 133 (2009)",
         ["139.2_lomo2"] = "BR 139.2 Lokomotion (2020)",
         ["139.2_lomo3"] = "BR 139.2 Lokomotion (2020)",
 
