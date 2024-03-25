22860d84812237efdd1df4e36ad4a185f5db11fa
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-03-25 20:09:55 +0100

    Cakv BR 140

--- strings.lua
+++ strings.lua
@@ -282,8 +282,8 @@ return {
         ["140.7_or2"] = "BR 140.7 Orientrot 2 DB (1987)",
         ["140.7_or3"] = "BR 140.7 Orientrot DB AG (1994)",
         ["140.7_or4"] = "BR 140.7 Orientrot 2 DB AG (1994)",
-        ["140.7_or4"] = "BR 140.7 Orientrot DB mit Cakv 69(1994)",
-        ["140.7_or4"] = "BR 140.7 Orientrot DB AG  mit Cakv 69 (1994)",
+        ["140.7_or5"] = "BR 140.7 Orientrot DB mit Cakv 69(1994)",
+        ["140.7_or6"] = "BR 140.7 Orientrot DB AG  mit Cakv 69 (1994)",
         ["140.7_vr"] = "BR 140.7 Verkehrsrot DB AG (1998)",	
 				
 		["e40_desc"] = "Die BR 140 war die Einheitselektrolokomotive für den Güterverkehr im 1950 erstellten Konzept der Deutschen Bundesbahn. Sie war lange Jahre die wichtigste Lokomotive in diesem Bereich mit zahlreichen Umbauten im Laufe des Leben. Da sie fast baugleich mit der BR 110 ist, wurde sie auch für den langsamen Personenverkehr genutzt. ",
